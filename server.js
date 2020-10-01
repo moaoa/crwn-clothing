ca06cfe0210c2c469518d6dcdeec4d444a8ce156
moaad <moaadbn3@gmial.com>
2020-10-01 11:25:21 +0200

    fixed server file

--- server.js
+++ server.js
@@ -18,10 +18,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
 }
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 app.post('/payments', (req, res) => {
     const body = {
